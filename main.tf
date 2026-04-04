72562b1698de34b541f75ca9aa8279d40efa952d
swathi <battulaswathi15407@gmail.com>
2026-04-05 00:52:37 +0530

    terraform

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ resource "aws_internet_gateway" "main" {
 
   tags = local.igw_final_tags
 }
+
+/*resource "aws_subnet" "pubilc" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+
+  tags = {
+    Name = "Main"
+  }
+} */
