b4f7184d56e296fbb0e77da3aac206b757290c66
swathi <battulaswathi15407@gmail.com>
2026-04-04 23:31:32 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "project" {
-    type =string
+    type = string
 }
 
 variable "environment" {
-    type = string
+    type = string   
 }
 
-variable  "vpc_cidr" {
+variable "vpc_cidr" {
     type = string
-    default  = "10.0.0.0/16"
+    default = "10.0.0.0/16"
 }
 
 variable "vpc_tags" {
