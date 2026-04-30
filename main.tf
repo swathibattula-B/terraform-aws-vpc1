e582aeacc43a93ccbb0df5e074a5bbc9bcf684c6
swathi <battulaswathi15407@gmail.com>
2026-04-30 17:33:41 +0530

    terraform

--- main.tf
+++ main.tf
@@ -168,3 +168,15 @@ resource "aws_route_table_association" "database" {
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
+
+resource "aws_db_subnet_group" "roboshop" {
+  name       = "${var.project}-${var.environment}"
+  subnet_ids = [aws_subnet.database[0].id,aws_subnet.database[1].id]
+
+  tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        }
+  )
+}
